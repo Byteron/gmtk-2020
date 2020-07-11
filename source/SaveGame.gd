9e195cf82910e711fc83d454f8a8d76180b497bc
Artificer 111 <danvernio@gmail.com>
2020-07-11 10:56:03 -0400

    gfx

---
+++ source/SaveGame.gd
@@ -0,0 +1,5 @@
+extends Resource
+class_name SaveGame
+
+export var version := 1
+export var data := {}
--- source/menu/TitleScreen.gd
+++ source/menu/TitleScreen.gd
@@ -1,9 +1,27 @@
 extends Control
 
 
+onready var continue_button := $CenterContainer/VBoxContainer/Continue
+
+
+func _ready() -> void:
+	if Global.current_level > 1:
+		continue_button.show()
+	else:
+		continue_button.hide()
+
+
 func _on_Play_pressed() -> void:
+	Global.current_level = 1
+	Global.save_game()
 	Scene.change("Level1")
+
+
+func _on_Continue_pressed() -> void:
+	var level = "Level%d" % Global.current_level
+	Scene.change(level)
 
 
 func _on_Quit_pressed() -> void:
 	get_tree().quit()
+
