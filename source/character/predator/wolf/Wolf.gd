cb43a3bb2783231f72811f8348150beea8817967
Aaron Winter <winter.aaron93@gmail.com>
2020-07-12 18:40:13 +0200

    disable wolf collision while eating

--- source/character/predator/wolf/Wolf.gd
+++ source/character/predator/wolf/Wolf.gd
@@ -2,6 +2,9 @@ extends Predator
 class_name Wolf
 
 
+onready var coll := $CollisionShape2D as CollisionShape2D
+
+
 func step() -> void:
 	SFX.play_sfx("AnimalFootstep")
 
--- source/character/predator/wolf/states/Eat.gd
+++ source/character/predator/wolf/states/Eat.gd
@@ -6,18 +6,20 @@ var target : Node2D = null
 
 
 func enter(host: Node) -> void:
-	var predator := host as Predator
-	predator.anim.play("bite")
+	var wolf := host as Wolf
+	wolf.coll.disabled = true
+	wolf.anim.play("bite")
 	SFX.play_sfx("WolfAttack")
-	yield(predator.anim, "animation_finished")
-	predator.anim.play("idle")
+	yield(wolf.anim, "animation_finished")
+	wolf.anim.play("idle")
 	SFX.play_sfx("WolfEat")
 	yield(get_tree().create_timer(8.0), "timeout")
-	predator.change_state("Patrol")
+	wolf.coll.disabled = false
+	wolf.change_state("Patrol")
 
 
 
 func exit(host: Node) -> void:
-	var predator := host as Wolf
+	var wolf := host as Wolf
 	target = null
 
