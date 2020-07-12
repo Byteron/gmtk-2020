74d992eb79c4f26cb2b14f23b3e19043d37c1f63
Aaron Winter <winter.aaron93@gmail.com>
2020-07-12 18:19:19 +0200

    wolfs bite to kill

--- source/character/predator/wolf/states/Eat.gd
+++ source/character/predator/wolf/states/Eat.gd
@@ -7,8 +7,11 @@ var target : Node2D = null
 
 func enter(host: Node) -> void:
 	var predator := host as Predator
-	SFX.play_sfx("WolfEat")
+	predator.anim.play("bite")
+	SFX.play_sfx("WolfAttack")
+	yield(predator.anim, "animation_finished")
 	predator.anim.play("idle")
+	SFX.play_sfx("WolfEat")
 	yield(get_tree().create_timer(8.0), "timeout")
 	predator.change_state("Patrol")
 
