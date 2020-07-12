7641882dc1f3de6d3e1e0317657f5cb7f86d779b
Aaron Winter <winter.aaron93@gmail.com>
2020-07-12 19:31:22 +0200

    hopefully fixing wolfs blocking each other while eating

--- source/character/predator/wolf/states/Eat.gd
+++ source/character/predator/wolf/states/Eat.gd
@@ -7,6 +7,7 @@ var target : Node2D = null
 
 func enter(host: Node) -> void:
 	var wolf := host as Wolf
+	call_deferred("disable", wolf)
 	wolf.coll.disabled = true
 	wolf.anim.play("bite")
 	SFX.play_sfx("WolfAttack")
@@ -14,9 +15,16 @@ func enter(host: Node) -> void:
 	wolf.anim.play("idle")
 	SFX.play_sfx("WolfEat")
 	yield(get_tree().create_timer(8.0), "timeout")
-	wolf.coll.disabled = false
+	call_deferred("enable", wolf)
 	wolf.change_state("Patrol")
 
+
+func enable(wolf: Wolf) -> void:
+	wolf.coll.disabled = false
+
+
+func disable(wolf: Wolf) -> void:
+	wolf.coll.disabled = true
 
 
 func exit(host: Node) -> void:
